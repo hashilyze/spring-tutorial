687e75bec27333a3fa9764899576a9b461986c6d
hashilyze <hashilyze@gmail.com>
2024-03-09 22:43:42 +0900

    Tutorial 2

---
+++ src/main/java/com/example/springtutorial/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.example.springtutorial.repository;
+
+import com.example.springtutorial.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ src/test/java/com/example/springtutorial/service/MemberServiceTest.java
@@ -0,0 +1,60 @@
+package com.example.springtutorial.service;
+
+import com.example.springtutorial.domain.Member;
+import com.example.springtutorial.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+    MemberService memberService;
+    MemoryMemberRepository memoryMemberRepository;
+
+    @BeforeEach
+    public void beforeEach(){
+        memoryMemberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memoryMemberRepository);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        memoryMemberRepository.clearStore();
+    }
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member();
+        member.setName("spring");
+        // when
+        Long saveId = memberService.join(member);
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+    }
+
+    @Test
+    void duplicateMemberException(){
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+        // when
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        // then
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
